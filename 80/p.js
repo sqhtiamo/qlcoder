e6950e88431ab48960e85dc87e6dbe1cb76c0292
ZYH <sqhtiamo@163.com>
2016-12-19 11:49:41 +0800

    add Problem 14

--- 80/p.js
+++ 80/p.js
@@ -9,7 +9,8 @@ let a=
 // {"level":17,"modu":"3","map":["11122","21102","10000","01112","11200"],"pieces":["..XX,.XX.,XX..,.X..","X.X.,XXXX,.X..","XXX,.X.,.X.","X...,X.XX,XXX.","X,X,X,X,X",".X...,XXXXX,X..X.","X..,XXX","XXX,X.X,X..","XX,X."]}
 // {"level":18,"modu":"2","map":["1010","0011","0101","1101","1110"],"pieces":["XX,.X",".X.,.X.,XXX","X.,XX,X.",".X..,XX..,.XXX,.XX.",".X.,XXX",".X..,XX..,XXXX,.X..",".X,.X,XX","X..,X..,XXX,XX.,X..","X.,X.,XX","X.,XX,.X"]}
 // {"level":19,"modu":"3","map":["0022","2102","1112","1111","2210"],"pieces":[".X,.X,XX,XX","XX,X.,X.","X.,XX","X..,XXX,.X.,.X.","XX,X.","XXX,..X","X.,X.,XX,.X,.X","XXXX","XX,X.","..X.,..X.,XXXX,...X"]}
-{"level":20,"modu":"2","map":["111011","001110","001000","110110","110100"],"pieces":["X.X.,XXX.,..XX,..X.","XX.,.X.,XXX,X..","X...,XXXX,.X..,.X..","....XX,XXXXX.,...X..,...X..","X..,X..,XXX","...X.,XXXXX,.X...,.X...","XXX,X..","XXX.,..XX,...X,..XX,..X.",".X,XX","XX,.X,.X"]}
+// {"level":20,"modu":"2","map":["111011","001110","001000","110110","110100"],"pieces":["X.X.,XXX.,..XX,..X.","XX.,.X.,XXX,X..","X...,XXXX,.X..,.X..","....XX,XXXXX.,...X..,...X..","X..,X..,XXX","...X.,XXXXX,.X...,.X...","XXX,X..","XXX.,..XX,...X,..XX,..X.",".X,XX","XX,.X,.X"]}
+{"level":23,"modu":"3","map":["00102","01112","10202","11212","01112","02202"],"pieces":[".X,XX,X.,X.","...X,...X,..XX,XXX.,X...","....X,XXXXX,X.X..,X....","..X..,..X..,.XX..,XXXX.,...XX",".X.,XXX,XX.,X..,XXX","..X,XXX,..X,..X",".X.,XXX,.XX,XXX,.X.","..X,XXX,X..,X..",".XX.,XXXX,X...","..X,XXX,.X.,.X.",".XX,.XX,XX."]}
 
 
 
